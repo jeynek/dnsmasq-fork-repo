919c93674b3adc80d262364194e5785ad549de3f
jeynek <jeynek>
2025-03-23 00:11:58 +0100

    deletion

--- dnsmasq-dhcp-server/dhcp-clients-webapp-backend/pkg/uibackend/doc.go
+++
@@ -1,3 +0,0 @@
-// This package implements an HTML/Websocket server that provides a tabular view of
-// all DHCP clients served by the dnsmasq instance that lives into this HomeAssistant addon
-package uibackend
